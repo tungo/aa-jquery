1826b3ed4e8f775ec3553d13eafd6bc84abe1ef4
Tu Ngo <tungo8810@gmail.com>
2017-05-01 14:05:41 -0700

    Complete tictactoe game

--- skeleton/js/bundle.js
+++ skeleton/js/bundle.js
@@ -330,20 +330,39 @@ class View {
       let col = Math.floor(idx / 3);
       let row = idx % 3;
 
-      this.game.playMove([row, col]);
-      this.makeMove(cell);
+      try {
+        this.game.playMove([row, col]);
+      } catch (e) {
+          return alert(e.msg);
+        }
+      this.makeMove(cell, event);
     });
   }
 
-  makeMove($square) {
+  makeMove($square, event) {
     $($square).html(this.game.currentPlayer);
     $($square).addClass('white');
+    $($square).addClass(this.game.currentPlayer);
+
+    if (this.game.isOver()) {
+      if (this.game.winner()) {
+        $('body').append($("<div>You win, " + this.game.currentPlayer + "</div>"));
+        $('ul li').addClass('loser');
+        $('.'+this.game.currentPlayer).addClass('winner');
+      } else {
+        $('body').append($("<div>Game is a draw</div>"));
+        $('ul li').addClass('loser');
+      }
+      $('ul li').off('click');
+      $('ul li').removeClass('player');
+
+    }
   }
 
   setupBoard() {
     let $ul = $("<ul></ul>");
     for (var i = 0; i < 9; i++) {
-      $ul.append($("<li></li>"));
+      $ul.append($("<li class='player'></li>"));
     }
     this.$el.append($ul);
   }
